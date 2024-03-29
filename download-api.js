640a31e4059426e3bcf41fca388137978294e341
BG Music <78909975+BG-Music@users.noreply.github.com>
2024-03-29 23:08:33 +0000

    Update download-api.js

--- download-api.js
+++ download-api.js
@@ -1,3 +1,14 @@
+// BG API Dowmload Info [DON'T CHANGE]
 let version = "v2.0"
+let prdouct = "BG Download API W/ Scanner"
+let updater = "firebase://bg-services/download-api-scanner/update-script.bgupdatepackage"
+
+// Scanner Config
 let type = "scanner"
 let site = "Virus Total-38573"
+
+// API Settings
+let apikey = "31894"
+let doaminLimitorEnabled = true
+
+// Copyright 2023 - 2024
